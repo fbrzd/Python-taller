854d7674bf68d5499a78b920d416b5ad2027995e
fbrzd <50089089+fbrzd@users.noreply.github.com>
2019-05-22 21:26:08 -0400

    fix activity

--- sesion-1/vs-pk.py
+++ sesion-1/vs-pk.py
@@ -1,3 +1,4 @@
+# valores iniciales
 pi_ataque = 15
 pi_vida = 15
 
@@ -9,14 +10,16 @@ turno = 0
 # bucle para el combate
 while pi_vida > 0 and ch_vida > 0:
 
+    print("turno",turno,"pikachu", pi_vida,"charmander", ch_vida)
     input()
-    input()
-
-    if turno == 0: # ataca charmander
-    	pi_vida = pi_vida - ch_atque # 15 - 10 = 5
     
-    if turno == 1: # atacar pikachu
-    	ch_vida = ch_vida - pi_ataque # 20 - 15 = 5
+    if turno == 0: # ataca charmander
+        print("ataca charmander")
+        pi_vida = pi_vida - ch_ataque # 15 - 10 = 5
+    
+    if turno == 1: # ataca pikachu
+        print("ataca pikachu")
+        ch_vida = ch_vida - pi_ataque # 20 - 15 = 5
 
     turno = 1 - turno
 
